d122118b9c1d4dbfa2fd669fba78a8a13e0e104a
Nikolay Gorb <knightvmk@users.noreply.github.com>
2016-12-02 16:39:50 +0300

    fix

--- Горб/virtual_cpu/virtual_cpu/queue.h
+++ Горб/virtual_cpu/virtual_cpu/queue.h
@@ -1,9 +1,9 @@
 #ifndef __QUEUE_H__
 #define __QUEUE_H__
 
-#define MAX_SIZE 100 // const for max size of queue (array)
+#define MAX_SIZE 100000 // const for max size of queue (array)
 #define MIN_SIZE 20 // const for min size of queue (array) // only for default constructor
-#define ALLOC_MEM 20 // const for realloc mem for add elements
+#define ALLOC_MEM 200 // const for realloc mem for add elements
 
 template <class type>
 class TQueue
@@ -13,7 +13,6 @@ private:
 	type *pMem; // mem for elem
 	size_t count; // count of elements in array 
 	size_t first;  // first elem in array
-	//size_t last; //last elem in array //actually last==count :( //why I have it??
 	void Realloc(); // realloc mem after pop
 
 public:
@@ -27,26 +26,17 @@ public:
 	bool IsEmpty() const;
 	bool IsFull() const;
 	size_t GetCount() const { return count; };
-	type GetFirst() const; // be or not to be? temp wasted method
 	type GetLast() const; // watch last elem
 	type Get(int i) const; //get i-elem
+	type Getd(int i); //get and del
 
 };
 
 template <class type>
 void TQueue<type>::Realloc()
 {
-	/*type *tmp = new type[size];
-	for (register int i = 0; i <= size; i++)
-		tmp[i] = pMem[i];
-	//delete[] pMem;*/
 	size += ALLOC_MEM;
 	pMem = (type*)realloc(pMem, ALLOC_MEM * sizeof(type));
-	//pMem = new type[size];
-	//for (register int i = 0,j=0; i <= size && j<=size; i++)
-		//pMem[i] = tmp[j];
-	//delete[] tmp;
-	//tmp = 0;
 }
 
 template <class type>
@@ -54,7 +44,8 @@ TQueue<type>::TQueue()
 {
 	size = MIN_SIZE;
 	pMem = new type[MIN_SIZE];
-	count = first = last = 0;
+	count = 0;
+	first = 1;
 }
 
 template <class type>
@@ -62,9 +53,10 @@ TQueue<type>::TQueue(const int _size)
 {
 	if (_size<1 || _size>MAX_SIZE)
 		throw ("Fail. Size too small or too big");
-	size = size_t(size);
+	size = size_t(_size);
 	pMem = new type[size+1];
-	count = first = 0;
+	count =0;
+	first = 1;
 }
 
 template <class type>
@@ -82,7 +74,6 @@ TQueue<type>::TQueue(const TQueue &a)
 	for (register int i = 0; i <= size; i++)
 		pMem[i] = a.pMem[i];
 	first = a.first;
-	//last = a.last;
 	count = a.count;
 }
 
@@ -90,11 +81,7 @@ template <class type>
 void TQueue<type>::Push(const type &R)
 {
 	if (IsFull()) Realloc();
-	//if (count!=first)
 	pMem[++count] = R;
-	first = 1;
-	//else pMem[count] = R;
-	//count++;
 }
 
 template <class type>
@@ -105,7 +92,6 @@ type TQueue<type>::Pop()
 		type res = pMem[first];
 		for (register int i = 0, j = 1; i <= count; i++)
 			pMem[i] = pMem[j++];
-		//last--;
 		count--;
 		return res;
 	}
@@ -135,14 +121,6 @@ bool TQueue<type>::IsEmpty() const
 }
 
 template <class type>
-type TQueue<type>::GetFirst() const //Attention, Warning, Alert, Achtung! //Temporary wasted method
-{
-	if (IsEmpty())
-		throw ("Fail. Queue is empty");
-		return pMem[first];
-}
-
-template <class type>
 type TQueue<type>::GetLast() const
 {
 	if (IsEmpty())
@@ -155,10 +133,24 @@ type TQueue<type>::Get(int i) const
 {
 	if (IsEmpty())
 		throw ("Fail. Queue is empty");
-	if (i>0 && i <= size)
+	if (i <= size)
 		return pMem[i];
 	else throw ("Fail. Incorrect iterator");
 }
 
+template <class type>
+type TQueue<type>::Getd(int i)
+{
+	if (IsEmpty())
+		throw ("Fail. Queue is empty");
+	if (i <= size)
+	{
+		type out = pMem[i];
+		for (register int k = i, j = i + 1; i <= count; i++)
+			pMem[k] = pMem[j];
+		return out;
+	}
+	else throw ("Fail. Incorrect iterator");
+}
 
 #endif
