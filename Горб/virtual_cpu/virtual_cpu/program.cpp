a9e9c7af0c86ab53bb449f2044ef91d4876647c8
Nikolay Gorb <knightvmk@users.noreply.github.com>
2016-12-02 12:11:41 +0300

    fix

--- Горб/virtual_cpu/virtual_cpu/program.cpp
+++ Горб/virtual_cpu/virtual_cpu/program.cpp
@@ -43,28 +43,29 @@ TProg::TProg(const int _count, const int _complex, int _size, int _cores) : TClu
 void TProg::Process()
 {
 	active = true;
-	double speed = SeeFreq()*0.0017;
+	double speed = 1000/SeeFreq();
 	int size_clust = SeeSize();
 	int percpu = SeeCorePerCPU();
 	tact = err = completes = 0;
 	bool gotcha = false;
-	TQueue<double> work(30);
+	TQueue<double> work(QUEUE);
 	srand(time(0)); //toggle randomizer
 	while (active)
 	{
-		for (register int i = 0; i < count*complex; i++, tact++)  //main cycle 
+		for (register int i = 0; i < count*complex || !work.IsEmpty(); i++, tact++)  //main cycle 
 		{
 			if (i < count)
 			{
 				alpha = double(rand()) / (RAND_MAX*(2 - 0) + 0); // alhpa == ( 0 <= a < 2 )
 				if (alpha > 1)
 				{
-					err++;
+					//err++;
 					break;
 				}//alpha must be (0 <= a < 1)
 				if (!work.IsFull()) //if queue is not full push task into, else denial of service
 				{
 					work.Push(task[i]); //load task
+					cache = work.GetCount();
 										//find free core:
 					gotcha = false;
 					for (register int p = 0; p < size_clust; p++)
@@ -84,16 +85,39 @@ void TProg::Process()
 
 					 //if free core exist:
 					if (gotcha) work.Pop();
-					else --i; //else wait (free cycle) - decrease iterator of cycle
+					else i--; //else wait (free cycle) - decrease iterator of cycle
 				}
 				else { err++; }
 			}
+			if (i >= count*complex && !work.IsEmpty())
+			{
+				gotcha = false;
+				for (register int p = 0; p < size_clust; p++)
+				{
+					for (register int t = 0; t < percpu; t++)
+					{
+						if (!IsBusy(p, t))
+						{
+							SetStat(p, t);
+							SetComplex(p, t, complex);
+							p = size_clust;
+							t = percpu;
+							gotcha = true;
+						}
+					}
+				}//end find free core
+
+				 //if free core exist:
+				if (gotcha) work.Pop();
+				else i--;
+			}
 			completes += CheckAll(); //count of complete tasks
-			Sleep(speed);
+			//Sleep(speed);
 
 
 		}
 		active = false;
+		cache = work.GetCount();
 	}
 	//PrintResults();
 }
@@ -110,6 +134,7 @@ void TProg::PrintResults()
 	printf("\n\n Current status of CPU's:");
 	printf("\n Size of Cluster: %i\n Count of cores: %i\n Total cores: %i", SeeSize(), SeeCorePerCPU(), SeeCores());
 	PrintStatus();
+	printf("\n Size of cache tasks: %i", cache);
 	printf("\n Count of completed tasks: %.0f", completes);
 	printf("\n Count of denial of service: %.0f", err);
 	printf("\n Count of used cluster's tacts: %.0f\n", tact);
